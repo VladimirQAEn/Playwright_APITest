8d4f6d0e865ab5d16467b45fed1819cf8fcc01ca
VladimirQAEn <qwer9889@proton.me>
2025-10-20 19:55:37 +0300

    post_test_1

---
+++ tests/post_api_request_01.spec.ts
@@ -0,0 +1,12 @@
+//Load playwright test module
+import { test, expect } from '@playwright/test';
+
+//Wright test
+
+//Create post request
+
+//Validate status code
+
+//Validate response body
+
+//Validate JSON object
